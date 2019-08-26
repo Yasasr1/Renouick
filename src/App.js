ceceb82ce301189e5e2e0fe92cf23c46a605ab14
PasanMahesha <Pmahesha.hera@gmail.com>
2019-08-26 14:36:26 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
-//import HomePage from './components/HomePage/HomePage';
-import CustomerRegistration from './components/RegistrationPages/CustomerRegistration/CustomerRegistration';
+import HomePage from './components/HomePage/HomePage';
+//import CustomerRegistration from './components/RegistrationPages/CustomerRegistration/CustomerRegistration';
 
 const app = () => {
   return (
     <div>
       <Layout>
-        <CustomerRegistration/>
+        <HomePage/>
       </Layout>
     </div>
   );
 }
+
 
 export default app;
