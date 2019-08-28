549c576d934b38377e08f271842cfabd4d8189a5
PasanMahesha <Pmahesha.hera@gmail.com>
2019-08-28 14:55:10 +0530

    FAQ

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
 //import HomePage from './components/HomePage/HomePage';
-import WorkerRegistration from './components/RegistrationPages/WorkerRegistration/WorkerRegistration';
+//import WorkerRegistration from './components/RegistrationPages/WorkerRegistration/WorkerRegistration';
+import FAQ from './components/FAQ/FAQ';
 
 const app = () => {
   return (
     <div>
       <Layout>
-        <WorkerRegistration/> 
+        <FAQ/> 
       </Layout>
     </div>
   );
