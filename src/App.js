b8f26a1cdc238315ff8e8b7f2d8f33e924b93e64
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2019-08-29 21:01:57 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
-import HomePage from './components/HomePage/HomePage';
-import WorkerRegistration from './components/RegistrationPages/WorkerRegistration/WorkerRegistration';
-import CustomerRegistration from './components/RegistrationPages/CustomerRegistration/CustomerRegistration';
+//import HomePage from './components/HomePage/HomePage';
+//import WorkerRegistration from './components/RegistrationPages/WorkerRegistration/WorkerRegistration';
+//import CustomerRegistration from './components/RegistrationPages/CustomerRegistration/CustomerRegistration';
+import Login from './components/LoginPage/LoginPage';
 
 const app = () => {
   return (
     <div>
       <Layout>
-        <WorkerRegistration/>
+        <Login/>
       </Layout>
     </div>
   );
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,15 +1,72 @@
 import { Component } from 'react';
 import React from 'react';
-import Grid from '@material-ui/core';
+import './LoginPage.css';
+import Typography from '@material-ui/core/Typography';
+import AccountBoxRoundedIcon from '@material-ui/icons/AccountBoxRounded';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+
+
 
 
 class LoginPage extends Component  {
    
     render() {
         return (
-            <Grid container Component="main">
-                
-            </Grid>
+            <React.Fragment>
+                <div className="LoginPageLeft">
+                </div>
+                <div className="LoginPageRight">
+                <AccountBoxRoundedIcon style={{fontSize: '80px', color: 'orange'}} />
+                <Typography component="h1" variant="h5">
+                     Sign in
+                </Typography>
+                <form>
+                    <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    />
+
+                    <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    />
+
+                    <br/>
+                    <br/>
+                    <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    >
+                        Sign In
+                    </Button>
+                    <br/>
+                    <br/>
+                    <Link href="#" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                    </Link>
+                </form>
+                </div>
+            </React.Fragment>
+            
+            
         );
     }
  
