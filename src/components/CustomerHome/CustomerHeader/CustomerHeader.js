9c7296f525e15e11c73274824c2062806e861163
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2019-09-13 08:49:42 +0530

    dashboard continued

--- src/components/CustomerHome/CustomerHeader/CustomerHeader.js
+++ src/components/CustomerHome/CustomerHeader/CustomerHeader.js
@@ -9,30 +9,49 @@ import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import DashboardIcon from '@material-ui/icons/Dashboard';
+import DashboardIcon  from '@material-ui/icons/Dashboard';
 import CreateIcon from '@material-ui/icons/Create';
 import HistoryIcon from '@material-ui/icons/History';
+import ChatIcon from '@material-ui/icons/Chat';
+import WorkIcon from '@material-ui/icons/Work';
+import Badge from '@material-ui/core/Badge';
+import MailIcon from '@material-ui/icons/Mail';
+import Avatar from '@material-ui/core/Avatar';
+//placeholder avatar
+import testAvatar from '../../../assests/testAvatar/avatar.jpg';
+
+
+
+
 
 import './CustomerHeader.css';
-import { ListItem } from '@material-ui/core';
+import { ListItem, Divider } from '@material-ui/core';
 
 class CustomerHeader extends Component  {
     state = {
         drawerOpen: false
     }
     
+    //toggle the open/close state of the side drawer
     toggleDrawer = (open) => {
 
         this.setState({drawerOpen: open});
     }
 
     render() {
+        //holds the item list that goes in the side drawer
         const itemList =  
             <div
             className="CustomerHeaderList"
             role="presentation"
             onClick={()=> this.toggleDrawer(false)}
             >
+                <Avatar alt="Yasas Ramanayaka" 
+                src={testAvatar} 
+                style={{height: '90px', width: '90px', alignSelf: 'center', margin: '20px'}}
+                />
+                <h6>Username</h6>
+                <Divider/>
                 <List>
                     <ListItem button>
                         <ListItemIcon>
@@ -42,7 +61,7 @@ class CustomerHeader extends Component  {
                     </ListItem>
                     <ListItem button>
                         <ListItemIcon>
-                            <CreateIcon/>
+                            <WorkIcon/>
                         </ListItemIcon>
                         <ListItemText primary="Post Job"/>
                     </ListItem>
@@ -52,20 +71,44 @@ class CustomerHeader extends Component  {
                         </ListItemIcon>
                         <ListItemText primary="Previous Jobs"/>
                     </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <ChatIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Messages"/>
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <CreateIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Edit Profile"/>
+                    </ListItem>
                 </List>
             </div>
     
         return (
             <div>
-                <AppBar position="static" className="CustomerHeader">
+                <AppBar position="static">
                     <Toolbar>
-                        <IconButton onClick={()=> this.toggleDrawer(true)} edge="start" color="inherit" style={{marginTop:'-20px'}}>
+                        <IconButton onClick={()=> this.toggleDrawer(true)} edge="start" color="inherit">
                             <MenuIcon/>
                         </IconButton>
-                        <Grid justify="flex-end" container>
-                            <IconButton color="inherit" style={{marginTop:'-20px'}}>
-                               <AccountCircle/>
-                            </IconButton>
+                        <Grid justify="flex-end" container spacing={2} >
+                            <Grid item>
+                                <IconButton>
+                                    <Badge badgeContent={5} color="secondary">
+                                        <MailIcon style={{color: 'white'}}/>
+                                    </Badge>
+                                </IconButton>
+                            </Grid>
+                            <Grid item>
+                                <h6 style={{marginTop: "10px"}}>Username</h6>
+                            </Grid>
+                            <Grid item>
+                                <IconButton color="inherit">
+                                   <AccountCircle/>
+                                </IconButton> 
+                            </Grid> 
                         </Grid>
                     </Toolbar>
                 </AppBar>
