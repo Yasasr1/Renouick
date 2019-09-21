9be49317d6221039e8aa50b14effbf5b5580a3cc
PasanMahesha <Pmahesha.hera@gmail.com>
2019-09-21 17:25:18 +0530

    team us

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout/Layout';
+<<<<<<< HEAD
+//import HomePage from './components/HomePage/HomePage';
+import WorkerRegistration from './components/RegistrationPages/WorkerRegistration/WorkerRegistration';
+import About from './components/About/About';
+const app = () => {
+  return (
+    <div>
+      <Layout>
+        <About/> 
+      </Layout>
+    </div>
+=======
 import CustomerHome from './components/CustomerHome/CustomerHome';
 
 
@@ -16,6 +28,7 @@ const app = () => {
       </div>
     </BrowserRouter>
     
+>>>>>>> 87bdc3e577016c731aa9d218d45ad90d880cbec2
   );
 }
 
