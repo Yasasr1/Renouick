f37debcdbaeac1b5481d0ccab2a6a42e18039f9d
Yasasr1 <2017is070@stu.ucsc.cmb.ac.lk>
2019-09-21 09:38:42 +0530

    1

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout/Layout';
-<<<<<<< HEAD
-//import HomePage from './components/HomePage/HomePage';
-import WorkerRegistration from './components/RegistrationPages/WorkerRegistration/WorkerRegistration';
-import About from './components/About/About';
-const app = () => {
-  return (
-    <div>
-      <Layout>
-        <About/> 
-      </Layout>
-    </div>
-=======
 import CustomerHome from './components/CustomerHome/CustomerHome';
 
 
@@ -28,7 +16,6 @@ const app = () => {
       </div>
     </BrowserRouter>
     
->>>>>>> 87bdc3e577016c731aa9d218d45ad90d880cbec2
   );
 }
 
