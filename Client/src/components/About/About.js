b5c38797f7c13e9a8001009d19c205a25af82ad4
thisaragimhani <36620106+thisaragimhani@users.noreply.github.com>
2019-11-12 12:45:04 +0530

    about commited

--- Client/src/components/About/About.js
+++ Client/src/components/About/About.js
@@ -14,50 +14,12 @@ const style = {
   }
 
 const About = () => {
-  //const [spacing, setSpacing] = React.useState(2);
-  const classes = useStyles();
-
-  /*const handleChange = event => {
-    setSpacing(Number(event.target.value));
-  };*/
-
-  return (
-    <Grid container className={classes.back} spacing={2}>
-        <Grid container className={classes.root} direction="row" justify="space-around" alignItems="center"  spacing={3}>
-
-              <Paper className={classes.paper}>
-              <Typography style={{ fontFamily:"Calibri " , fontSize:24 , fontStyle:"Italic" , color:"white"}}> Renouick is a way to connect customers who are not capable of finding services and workers who are not capable of promoting their services.</Typography>
-              </Paper>
-             <Paper className={classes.paper}>
-              <Typography style={{ fontFamily:"Calibri " , fontSize:24 , fontStyle:"Italic" , color:"white"}}> In the current society people face problems in finding workers for their day to day problems.</Typography>
-              </Paper>
-     
-              <Paper className={classes.paper}>
-              <Typography style={{ fontFamily:"Calibri " , fontSize:24 , fontStyle:"Italic" , color:"white"}}> Even if many workers are available if they cannot be founded when needed there is no use.</Typography>
-              </Paper>
-            
-      </Grid>
-      <Grid container className={classes.root} direction="row" justify="space-around" alignItems="center" spacing={3} >
-
-            <Paper className={classes.paper}>
-            <Typography style={{ fontFamily:"Calibri " , fontSize:24 , fontStyle:"Italic", align:"center" , color:"white"}}>That is why we provided a solution by introducing Renouick.</Typography>
-            </Paper>
-
-            <Paper className={classes.paper}>
-            <Typography style={{ fontFamily:"Calibri " , fontSize:24 , fontStyle:"Italic" , color:"white"}}> Renouick is a way to connect customers who are not capable of finding services and workers who are not capable of promoting their services.</Typography>
-            </Paper>
-
-
-</Grid>
-     </Grid>
-  );
-}
 
     return(
         <Paper style={style.paperContainer}>
         <div className="AboutPage" style={{padding: '40px', fontStyle:"normal"}}>
            
-           <h1 style={{ fontFamily:"Calibri " , fontSize:50 , fontStyle:"Italic" , color:"brown"}}>Welcome to Renouick!</h1>
+           <h1 style={{ fontFamily:"Calibri " , fontSize:50 , fontStyle:"Italic" , color:"orange"}}>Welcome to Renouick!</h1>
                 <h2>What is Renoick?</h2>
                 <div style={{fontSize:18}} >
         <p>Renouick is a way to connect customers and workers. </p>
@@ -85,3 +47,4 @@ const About = () => {
 };
 
 export default About;
+
