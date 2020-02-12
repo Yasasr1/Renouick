8fa9eb74ab8f01620c674b3d6bf4f9ff4e9c5d02
Yasas Ramanayaka <36619815+Yasasr1@users.noreply.github.com>
2020-02-12 23:08:43 +0530

    chat

--- Client/src/components/WorkerHome/ViewJob/ViewJob.js
+++ Client/src/components/WorkerHome/ViewJob/ViewJob.js
@@ -1,11 +1,15 @@
 //display a single job after clicking on it in the list
 import React, { useState } from 'react';
-import { Grid, Divider, Button, Chip, Typography, TextField, Snackbar, IconButton } from '@material-ui/core';
+import { Grid, Divider, Button, Chip, Typography, TextField, Snackbar, IconButton,Dialog,DialogTitle } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import { Carousel } from 'react-bootstrap';
+import { Carousel,Modal } from 'react-bootstrap';
 import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 import axios from 'axios';
 import { connect } from 'react-redux';
+import { StreamChat } from 'stream-chat';
+import { Chat, Channel, ChannelHeader, Thread, Window, MessageList, MessageInput } from 'stream-chat-react';
+
+
 
 
 
@@ -15,6 +19,10 @@ const ViewJob = (props) => {
     const[amount,setAmount] = useState(0);
     //for opening the snackbar after bid is placed
     const[open,setOpen] = useState(false);
+    //popup
+    const [show, setShow] = useState(false);
+
+    
     //responce after bid is placeed
     const[message,setMessage] = useState("");
 
@@ -42,8 +50,7 @@ const ViewJob = (props) => {
         }
     
         setOpen(false);
-      };
-    
+    };
 
     const placeBIdHandler = () => {
         const bid = {
@@ -66,6 +73,67 @@ const ViewJob = (props) => {
             setMessage("Bid placing failed");
         })
     }
+
+    //chat
+    const client = new StreamChat("vpwfkwnszegg");
+    const userToken = props.chatToken;
+
+    const workeremail = props.email;
+    var n = workeremail.indexOf("@");
+    var workername = workeremail.slice(0, n);
+    //console.log(name);
+
+    const customeremail = props.location.state.poster;
+    var m = customeremail.indexOf("@");
+    var customername = customeremail.slice(0, m);
+
+    var channelName = workername.concat('-',customername);
+    console.log(channelName);
+
+    //client.disconnect();
+    client.setUser(
+        {
+            id: workername,
+            name: workername,
+            image: 'https://i.imgur.com/fR9Jz14.png',
+        },
+        userToken,
+    );
+
+    const conversation = client.channel('messaging', channelName, {
+        name: channelName,
+        image: 'http://bit.ly/2O35mws',
+        members: [workername, 'pasan']
+    });
+
+
+    //for holding the chat window
+
+    let chatWindow;
+    if(show === false){
+        chatWindow = null;
+    }
+    else
+        chatWindow = <Chat client={client} theme={'messaging light'}>
+                        <Channel channel={conversation}>
+                            <Window>
+                            <ChannelHeader />
+                            <MessageList />
+                            <MessageInput />
+                            </Window>
+                            <Thread />
+                        </Channel>
+                    </Chat>
+    
+    const handlePopupClose = () => {
+        setShow(false)
+    };
+    const handlePopupShow = () => {
+        setShow(true)
+
+    };
+
+
     //{props.location.state.title}
     return (
         <Grid style={{margin: '100px', padding: '20px'}} container spacing={2}>
@@ -90,6 +158,12 @@ const ViewJob = (props) => {
                 <p className="h6">Posted By : {props.location.state.poster}</p>
                 <p className="h6">Poste Date : {date}</p>
                 <p className="h6">Contact Number : {props.location.state.contactNumber}</p>
+            </Grid>
+            <Grid item md={12}>
+                <Button onClick={handlePopupShow}>Chat</Button>
+                <Modal style={{marginTop: '30px', marginBottom: '50px'}} show={show} onHide={handlePopupClose}>
+                    {chatWindow}
+                </Modal>
             </Grid>
             <Grid item md={12}>
                 <Divider/>
@@ -137,6 +211,7 @@ const ViewJob = (props) => {
 const mapStateToProps = state => {
     return {
         email: state.email,
+        chatToken: state.chatToken
         
     }
 }
